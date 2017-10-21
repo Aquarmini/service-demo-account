f6312d4a7500cc4b3b7b15bb343c6e554153746e
李铭昕 <715557344@qq.com>
2017-10-21 17:18:47 +0800

    * 增加个人中心

--- vue/src/app/components/Index.vue
+++ vue/src/app/components/Index.vue
@@ -1,8 +1,44 @@
 <template>
     <div id="index">
-        <img src="../../assets/logo.png">
-        <div>{{msg}}</div>
-        <a href="javascript:;" class="weui-btn weui-btn_primary">绿色按钮</a>
+        <div class="weui-flex">
+            <div class="weui-flex__item">
+                <div class="placeholder">
+                    <div class="weui-cells">
+                        <div class="weui-cell">
+                            <div class="weui-cell__hd" style="position: relative;margin-right: 10px;">
+                                <img src="../../assets/logo.png" style="width: 50px;display: block">
+                                <span class="weui-badge" style="position: absolute;top: -.4em;right: -.4em;">8</span>
+                            </div>
+                            <div class="weui-cell__bd">
+                                <p>联系人名称</p>
+                                <p style="font-size: 13px;color: #888888;">摘要信息</p>
+                            </div>
+                        </div>
+                        <div class="weui-cell weui-cell_access">
+                            <div class="weui-cell__bd">
+                                <span style="vertical-align: middle">单行列表</span>
+                                <span class="weui-badge" style="margin-left: 5px;">8</span>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </div>
+                        <div class="weui-cell weui-cell_access">
+                            <div class="weui-cell__bd">
+                                <span style="vertical-align: middle">单行列表</span>
+                                <span class="weui-badge" style="margin-left: 5px;">8</span>
+                            </div>
+                            <div class="weui-cell__ft">详细信息</div>
+                        </div>
+                        <div class="weui-cell weui-cell_access">
+                            <div class="weui-cell__bd">
+                                <span style="vertical-align: middle">单行列表</span>
+                                <span class="weui-badge" style="margin-left: 5px;">New</span>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
