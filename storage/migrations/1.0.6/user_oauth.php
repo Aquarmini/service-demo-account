73309acffaa1ae03e3d445475d58e87bfd335a10
李铭昕 <715557344@qq.com>
2017-10-28 13:41:21 +0800

    * 增加体重表

---
+++ storage/migrations/1.0.6/user_oauth.php
@@ -0,0 +1,126 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class UserOauthMigration_106
+ */
+class UserOauthMigration_106 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('user_oauth', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 20,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'user_id',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'default' => "0",
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 20,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'default' => "",
+                            'notNull' => true,
+                            'size' => 64,
+                            'after' => 'user_id'
+                        ]
+                    ),
+                    new Column(
+                        'code',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'default' => "",
+                            'notNull' => true,
+                            'size' => 128,
+                            'after' => 'name'
+                        ]
+                    ),
+                    new Column(
+                        'type',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "0",
+                            'notNull' => true,
+                            'size' => 4,
+                            'after' => 'code'
+                        ]
+                    ),
+                    new Column(
+                        'created_at',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'type'
+                        ]
+                    ),
+                    new Column(
+                        'updated_at',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'created_at'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('TYPE_CODE_UNIQUE', ['type', 'code'], 'UNIQUE'),
+                    new Index('USER_ID_INDEX', ['user_id'], null)
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '3',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8mb4_unicode_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ storage/migrations/1.0.6/weight.php
@@ -0,0 +1,116 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class WeightMigration_106
+ */
+class WeightMigration_106 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('weight', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 20,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'user_id',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'default' => "0",
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 20,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'date',
+                        [
+                            'type' => Column::TYPE_DATE,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'user_id'
+                        ]
+                    ),
+                    new Column(
+                        'weight',
+                        [
+                            'type' => Column::TYPE_DECIMAL,
+                            'default' => "0.00",
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 5,
+                            'scale' => 2,
+                            'after' => 'date'
+                        ]
+                    ),
+                    new Column(
+                        'created_at',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'weight'
+                        ]
+                    ),
+                    new Column(
+                        'updated_at',
+                        [
+                            'type' => Column::TYPE_DATETIME,
+                            'size' => 1,
+                            'after' => 'created_at'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('USER_DATE_UNIQUE', ['user_id', 'date'], 'UNIQUE')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8mb4_unicode_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
